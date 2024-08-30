26d2ac26f5dfb686682d707f0147a458e2002ad2
Vinicius Pinheiro <vinicius.pinheiro@dxc.com>
2024-08-30 16:00:43 -0300

    feat: corrigindo comportamento do chat

--- js/comunicacao_gemini.js
+++ js/comunicacao_gemini.js
@@ -1,6 +1,8 @@
-document.getElementById("chat-form").addEventListener("submit", function(event) {
+document
+  .getElementById("chat-form")
+  .addEventListener("submit", function (event) {
     const API_URL = "http://localhost:8080/api/gemini/enviar-pergunta";
-    const API_KEY = "AIzaSyDZDueKsWFGwOt2IY5vGFt_dK3fmFC6fts";
+    const API_KEY = "YOUR_GOOGLE_API_KEY";
 
     event.preventDefault(); // Impede o envio padrão do formulário
 
@@ -41,43 +43,82 @@ document.getElementById("chat-form").addEventListener("submit", function(event) 
 
     // Envia a mensagem para a API
     setTimeout(() => {
-        fetch(API_URL, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ pergunta: message, apiKey: API_KEY })
-        })
-        .then(response => response.json())
-        .then(data => {
-            // Remove a mensagem "digitando..."
-            const typingIndicator = document.querySelector('.typing-indicator');
-            if (typingIndicator) {
-                typingIndicator.remove();
-            }
-    
-            // Adiciona a resposta da API ao chat
-            let botMessage = `
+      fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ nroChamado: message, apiKey: API_KEY }),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          // Remove a mensagem "digitando..."
+          const typingIndicator = document.querySelector(".typing-indicator");
+          if (typingIndicator) {
+            typingIndicator.remove();
+          }
+
+          if (data.mensagem) {
+            // Transformar cada '\n\n' em uma nova mensagem:
+            data.mensagem.split("\n\n").forEach((msg) => {
+              // Adiciona a resposta da API ao chat
+              const botMessage = `
                 <div class="message message-left">
                     <div class="avatar-wrapper avatar-small">
                         <img src="./img/Coda.png" alt="avatar" />
                     </div>
                     <div class="bubble bubble-light">
-                        ${data.mensagem}
+                        ${msg.replace(/\n/g, "<br />")}
                     </div>
                 </div>
-            `;
+                `;
+              chatRoom.innerHTML += botMessage;
+            });
+
+            const botMessage = `
+                <div class="message message-left">
+                    <div class="avatar-wrapper avatar-small">
+                        <img src="./img/Coda.png" alt="avatar" />
+                    </div>
+                    <div class="bubble bubble-light">
+                        Gostaria de ajuda em outros chamados? Digite um novo chamado para continuar.
+                    </div>
+                </div>
+                `;
             chatRoom.innerHTML += botMessage;
-    
-            // Rola o chat para a última mensagem
-            chatRoom.scrollTop = chatRoom.scrollHeight;
-        })
-        .catch(error => {
-            console.error("Erro:", error);
+          } else {
+            if (data.status === 404) {
+              const botMessage = `
+                    <div class="message message-left">
+                        <div class="avatar-wrapper avatar-small">
+                            <img src="./img/Coda.png" alt="avatar" />
+                        </div>
+                        <div class="bubble bubble-light">
+                            Chamado não encontrado. Por favor, entre na aba "Chamados" e selecione um número de chamado válido.
+                        </div>
+                    </div>
+                    `;
+              chatRoom.innerHTML += botMessage;
+            } else if (data.status === 403) {
+              const botMessage = `
+                      <div class="message message-left">
+                          <div class="avatar-wrapper avatar-small">
+                              <img src="./img/Coda.png" alt="avatar" />
+                          </div>
+                          <div class="bubble bubble-light">
+                              API KEY inválida. Verique se sua API esta setada corretamente no seu código.
+                          </div>
+                      </div>
+                      `;
+              chatRoom.innerHTML += botMessage;
+            }
+          }
+
+          // Rola o chat para a última mensagem
+          chatRoom.scrollTop = chatRoom.scrollHeight;
         });
-    }, 2000)
-    
+    }, 1500);
 
     // Limpa o campo de entrada de texto
     document.getElementById("inputText").value = "";
-});
+  });
--- js/tabela_chamados.js
+++ js/tabela_chamados.js
@@ -1,16 +1,15 @@
-const API_URL = "http://localhost:8080/api/chamados"
-
+const API_URL = "http://localhost:8080/api/chamados";
 
 // Tabela de Chamados Abertos
 fetch(`${API_URL}/em-fila`)
-.then(function(response){
-  return response.json();
-})
-.then(function(chamados){
-  let placeholder = document.querySelector("#chamados_abertos");
-  let out = "";
-  for( let chamado of chamados){
-    out += `
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (chamados) {
+    let placeholder = document.querySelector("#chamados_abertos");
+    let out = "";
+    for (let chamado of chamados) {
+      out += `
       <tr>
           <td class="align-middle text-center text-sm">
               <span class="badge badge-sm bg-danger">NA FILA</span>
@@ -19,24 +18,22 @@ fetch(`${API_URL}/em-fila`)
               <span class="text-secondary text-xs font-weight-bold">${chamado.aberto}</span>
           </td>
           <td>
-              <a href="">
-                  <h6 class="mb-0 text-sm">${chamado.numero}</h6>
-                </a>
+              <h6 class="mb-0 text-sm">${chamado.numero}</h6>
           </td>
           <td class="align-middle text-center">
               <span class="text-secondary text-xs font-weight-bold">${chamado.descricao}</span>
           </td>
       </tr>
     `;
-  }
-  placeholder.innerHTML += out;
-})
+    }
+    placeholder.innerHTML += out;
+  });
 
 fetch(`${API_URL}/em-tratamento`)
-  .then(function(response) {
+  .then(function (response) {
     return response.json();
   })
-  .then(function(chamados) {
+  .then(function (chamados) {
     let placeholder = document.querySelector("#chamados_abertos");
     let out = "";
     for (let chamado of chamados) {
@@ -49,9 +46,7 @@ fetch(`${API_URL}/em-tratamento`)
             <span class="text-secondary text-xs font-weight-bold">${chamado.aberto}</span>
           </td>
           <td>
-            <a href="">
-              <h6 class="mb-0 text-sm">${chamado.numero}</h6>
-            </a>
+            <h6 class="mb-0 text-sm">${chamado.numero}</h6>
           </td>
           <td class="align-middle text-center">
             <span class="text-secondary text-xs font-weight-bold">${chamado.descricao}</span>
@@ -61,21 +56,20 @@ fetch(`${API_URL}/em-tratamento`)
     }
     placeholder.innerHTML += out;
   })
-  .catch(function(error) {
+  .catch(function (error) {
     console.error("Erro ao buscar os chamados:", error);
   });
 
-
 // Tabela de Chamados Pendentes
 fetch(`${API_URL}/em-validacao`)
-.then(function(response){
-  return response.json();
-})
-.then(function(chamados){
-  let placeholder = document.querySelector("#chamados_pendentes");
-  let out = "";
-  for( let chamado of chamados){
-    out += `
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (chamados) {
+    let placeholder = document.querySelector("#chamados_pendentes");
+    let out = "";
+    for (let chamado of chamados) {
+      out += `
       <tr>
         <td class="align-middle text-center text-sm">
           <span class="badge badge-sm bg-dark">REQUER VALIDAÇÃO</span>
@@ -84,29 +78,27 @@ fetch(`${API_URL}/em-validacao`)
           <span class="text-secondary text-xs font-weight-bold">${chamado.aberto}</span>
         </td>
         <td>
-          <a href="">
-            <h6 class="mb-0 text-sm">${chamado.numero}</h6>
-          </a>
+          <h6 class="mb-0 text-sm">${chamado.numero}</h6>
         </td>
         <td class="align-middle text-center">
           <span class="text-secondary text-xs font-weight-bold">${chamado.descricao}</span>
         </td>
       </tr>
     `;
-  }
-  placeholder.innerHTML += out;
-})
+    }
+    placeholder.innerHTML += out;
+  });
 
 // Tabela de Chamados Encerrados
 fetch(`${API_URL}/resolvidos`)
-.then(function(response){
-  return response.json();
-})
-.then(function(chamados){
-  let placeholder = document.querySelector("#chamados_encerrados");
-  let out = "";
-  for( let chamado of chamados){
-    out += `
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (chamados) {
+    let placeholder = document.querySelector("#chamados_encerrados");
+    let out = "";
+    for (let chamado of chamados) {
+      out += `
       <tr>
         <td class="align-middle text-center text-sm">
           <span class="badge badge-sm bg-success">RESOLVIDO</span>
@@ -115,15 +107,13 @@ fetch(`${API_URL}/resolvidos`)
           <span class="text-secondary text-xs font-weight-bold">${chamado.aberto}</span>
         </td>
         <td>
-          <a href="">
-            <h6 class="mb-0 text-sm">${chamado.numero}</h6>
-          </a>
+          <h6 class="mb-0 text-sm">${chamado.numero}</h6>
         </td>
         <td class="align-middle text-center">
           <span class="text-secondary text-xs font-weight-bold">${chamado.descricao}</span>
         </td>
       </tr>
     `;
-  }
-  placeholder.innerHTML += out;
-})
+    }
+    placeholder.innerHTML += out;
+  });
